2124d0c86d11ee37e6f650092e78fda41a2df847
phillausofia <47079420+phillausofia@users.noreply.github.com>
2019-10-16 16:42:39 +0300

    Add a MapView with a marker with the earthquake coordinates.

--- app/src/main/java/com/earthquakesaroundme/detail/DetailFragment.kt
+++ app/src/main/java/com/earthquakesaroundme/detail/DetailFragment.kt
@@ -11,16 +11,24 @@ import com.earthquakesaroundme.R
 import com.earthquakesaroundme.databinding.FragmentDetailBinding
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.MapView
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import kotlinx.android.synthetic.main.fragment_detail.*
 
 class DetailFragment : Fragment(), OnMapReadyCallback {
 
     private lateinit var viewModel: DetailViewModel
+    private lateinit var mapView: MapView
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView.onSaveInstanceState(outState)
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?): View? {
+                              savedInstanceState: Bundle?): View? {
 
         val binding = DataBindingUtil.inflate<FragmentDetailBinding>(inflater,
             R.layout.fragment_detail, container, false)
@@ -35,7 +43,11 @@ class DetailFragment : Fragment(), OnMapReadyCallback {
 
         binding.viewModel = viewModel
 
-        binding.earthquakeLocationMapView.getMapAsync(this)
+        mapView = binding.earthquakeLocationMapView
+
+        mapView.onCreate(savedInstanceState)
+
+        mapView.getMapAsync(this)
 
 
         return binding.root
@@ -44,8 +56,39 @@ class DetailFragment : Fragment(), OnMapReadyCallback {
 
     override fun onMapReady(p0: GoogleMap?) {
         val (lat, lng) = viewModel.getEarthquakeCoordinates()
-        p0!!.addMarker(MarkerOptions().position(LatLng(lat, lng)))
-        p0!!.moveCamera(CameraUpdateFactory.newLatLng(LatLng(lat, lng)))
+        p0!!.addMarker(MarkerOptions().position(LatLng(lat, lng)).title(viewModel.
+            selectedEarthquake.value!!.properties.place))
+        p0.moveCamera(CameraUpdateFactory.newLatLng(LatLng(lat, lng)))
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapView.onStart()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
     }
 
 }
