6fc6c3f255dbde7d44a7288db3a3560d350df952
phillausofia <47079420+phillausofia@users.noreply.github.com>
2019-10-22 13:31:58 +0300

    Add explanatory comments.

--- app/src/main/java/com/earthquakesaroundme/search_options/SearchOptionsFragment.kt
+++ app/src/main/java/com/earthquakesaroundme/search_options/SearchOptionsFragment.kt
@@ -32,42 +32,19 @@ class SearchOptionsFragment: Fragment() {
 
         val application = requireNotNull(activity).application
 
-        interstitialAd = InterstitialAd(context!!)
-        interstitialAd.adUnitId = "ca-app-pub-3940256099942544/1033173712"
-        if (!interstitialAd.isLoaded) {
-            interstitialAd.loadAd(AdRequest.Builder().build())
-        }
-        interstitialAd.adListener = object : AdListener() {
-            override fun onAdClosed() {
-                interstitialAd.loadAd(AdRequest.Builder().build())
-            }
-        }
-        binding = DataBindingUtil.inflate<FragmentSearchOptionsBinding>(inflater,
-            R.layout.fragment_search_options, container, false)
+//        initInterstitialAd()
+
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_search_options,
+            container, false)
 
         val viewModelFactory = SearchOptionsViewModelFactory(application)
 
         viewModel = ViewModelProviders.of(this, viewModelFactory)
             .get(SearchOptionsViewModel::class.java)
 
-        /*
-        The search options for a parameter will appear only if the user wants to set that
-        specific search parameter
-         */
         showInputFields()
 
-        //Initializing the number pickers
-        binding.numberPickerMinMagnitude.initializeNumberPicker()
-        binding.numberPickerMaxMagnitude.initializeNumberPicker()
-        if (viewModel.minMag.value != null) {
-            binding.numberPickerMinMagnitude.value = viewModel.minMag.value!!
-        }
-        if (viewModel.maxMag.value != null) {
-            binding.numberPickerMaxMagnitude.value = viewModel.maxMag.value!!
-        }
-
-
-
+        initNumberPickers()
 
         binding.buttonSearch.setOnClickListener { view ->
             val searchOptions = getSearchOptions(binding)
@@ -76,8 +53,6 @@ class SearchOptionsFragment: Fragment() {
                     searchOptions.endTime)) {
 //                if (interstitialAd.isLoaded) {
 //                    interstitialAd.show()
-//                } else {
-//                    Log.d("onCreateView", "Ad was not loaded.")
 //                }
                 view.findNavController()
                     .navigate(
@@ -91,21 +66,53 @@ class SearchOptionsFragment: Fragment() {
         return binding.root
     }
 
+    private fun initNumberPickers() {
+        //Initializing the number pickers
+        binding.numberPickerMinMagnitude.initializeNumberPicker()
+        binding.numberPickerMaxMagnitude.initializeNumberPicker()
+        //If a previous value of the number pickers is saved, then we should use it
+        if (viewModel.minMag.value != null) {
+            binding.numberPickerMinMagnitude.value = viewModel.minMag.value!!
+        }
+        if (viewModel.maxMag.value != null) {
+            binding.numberPickerMaxMagnitude.value = viewModel.maxMag.value!!
+        }
+    }
+
+    private fun initInterstitialAd() {
+        interstitialAd = InterstitialAd(context!!)
+        interstitialAd.adUnitId = "ca-app-pub-3940256099942544/1033173712"
+        if (!interstitialAd.isLoaded) {
+            interstitialAd.loadAd(AdRequest.Builder().build())
+        }
+        interstitialAd.adListener = object : AdListener() {
+            override fun onAdClosed() {
+                interstitialAd.loadAd(AdRequest.Builder().build())
+            }
+        }
+    }
+
     private fun NumberPicker.initializeNumberPicker() {
         minValue = SearchOptionsUtils.EARTHQUAKE_MAGNITUDE_RANGE.MIN_MAGNITUDE
         maxValue = SearchOptionsUtils.EARTHQUAKE_MAGNITUDE_RANGE.MAX_MAGNITUDE
         wrapSelectorWheel = true
     }
 
+    /*
+    We get the value of a search parameter only if the CheckBox assigned to that parameter
+    is checked
+     */
     private fun getSearchOptions(
         binding: FragmentSearchOptionsBinding) : SearchOptions {
 
         val minMag = if (binding.checkBoxMinMag.isChecked)
             binding.numberPickerMinMagnitude.value else null
-        viewModel.setMinMag(minMag)
         val maxMag = if (binding.checkBoxMaxMag.isChecked)
             binding.numberPickerMaxMagnitude.value else null
+        //We save the values of the number pickers so we can use it when the fragment is restored
+        //from the back stack
         viewModel.setMaxMag(maxMag)
+        viewModel.setMinMag(minMag)
         val orderBy = if (binding.checkBoxOrderBy.isChecked)
             getOrderBySearchOption(binding) else null
         val maxRadiusKm = if (binding.checkBoxMaxRadiusKm.isChecked)
@@ -118,6 +125,10 @@ class SearchOptionsFragment: Fragment() {
 
     }
 
+    /*
+    Returns the value of the orderBy parameter depending on the
+    selected option from the radio group
+     */
     private fun getOrderBySearchOption(binding: FragmentSearchOptionsBinding) =
         when(binding.orderBySearchOptionsRadioGroup.checkedRadioButtonId) {
             binding.timeAscRadioButton.id -> SearchOptionsUtils.UsgsApiOrderByValues.TIME_ASC
@@ -147,9 +158,10 @@ class SearchOptionsFragment: Fragment() {
                     Toast.LENGTH_SHORT).show()
                 return false
             }
+            //The end date must also be after the start date
             val endDate = toDate(endTime)
             val startDate = if (startTime != null) toDate(startTime) else
-                Date(Date().time - 30L * 24 * 3600 * 1000)
+                Date(Date().time - 30L * 24 * 3600 * 1000) //by default is set to NOW - 30 days
             if (!endDate.after(startDate)) {
                 Toast.makeText(this.context, getString(R.string.end_date_after_start_date), Toast.LENGTH_SHORT).show()
                 return false
@@ -158,8 +170,11 @@ class SearchOptionsFragment: Fragment() {
         return true
     }
 
+    /*
+    The string date is valid if we don't get an exception
+    when we try to create a data object with it
+     */
     private fun isDateCorrect(dateString: String): Boolean {
-
         return try {
             toDate(dateString)
             true
@@ -167,10 +182,9 @@ class SearchOptionsFragment: Fragment() {
             false
         }
     }
-
     private fun toDate(dateString: String) : Date {
         try {
-            return SimpleDateFormat("yyyy-MM-dd").apply {
+            return SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).apply {
                 isLenient = false
             }.parse(dateString)
         } catch (e: Exception) {
@@ -178,6 +192,10 @@ class SearchOptionsFragment: Fragment() {
         }
     }
 
+    /*
+    The input field for a parameter will appear only if the user wants to set that
+    specific search parameter
+     */
     private fun showInputFields() {
         setViewVisibility(binding.checkBoxMinMag, viewModel.setIsCheckBoxMinMagChecked,
             viewModel.isCheckBoxMinMagChecked, binding.numberPickerMinMagnitude)
@@ -191,17 +209,22 @@ class SearchOptionsFragment: Fragment() {
             viewModel.isCheckBoxStartDateChecked, binding.editTextStartDate)
         setViewVisibility(binding.checkBoxEndDate, viewModel.setIsCheckBoxEndDateChecked,
             viewModel.isCheckBoxEndDateChecked, binding.editTextEndDate)
-
     }
 
-    fun setViewVisibility(checkBox: CheckBox, setValue: (Boolean) -> Unit,
+    /*
+    If the corresponding checkBox is checked, then we show the View representing the input field
+     */
+    private fun setViewVisibility(checkBox: CheckBox, setValue: (Boolean) -> Unit,
                           isCheckBoxChecked: LiveData<Boolean>, viewToMakeVisible: View) {
         setClickListener(checkBox, setValue)
         isCheckBoxChecked.observe(this, androidx.lifecycle.Observer {
                 isChecked -> viewToMakeVisible.visibility = if (isChecked) View.VISIBLE else View.GONE
         })
     }
-
+    /*
+    We save the value that says whether a CheckBox is checked or not so we can show (or not) the
+    input field when the fragment is restored from the back-stack
+     */
     private fun setClickListener(checkBox: CheckBox, setValue: (Boolean) -> Unit) {
         checkBox.setOnClickListener {
             setValue(checkBox.isChecked)
